d239da99870f3b901ff68a3d5dd82a8680f6590a
joefarah850 <joefarah850@gmail.com>
2023-07-03 22:46:16 +0300

    v4.0

---
+++ F1 Schedule/js/script.js
@@ -0,0 +1,123 @@
+var messageDiv = document.getElementById('message');
+
+function loginListener() {
+
+    var loginForm = document.getElementById('loginForm');
+
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        var lEmailAddressInput = document.getElementById('l-emailAddress');
+        var lPasswordInput = document.getElementById('l-password');
+        var lFormData = {
+            emailAddress: lEmailAddressInput.value,
+            password: lPasswordInput.value
+        };
+
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:5000/login', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                if (response.message === 0) {
+                    messageDiv.textContent = 'Incorrect email or password!';
+                    messageDiv.style.color = 'red';
+                    lEmailAddressInput.value = "";
+                    lPasswordInput.value = "";
+                } else {
+                    goToHomePage();
+                }
+                
+            } else {
+                messageDiv.textContent = 'Oops! An error occurred.';
+            }
+        };
+
+        xhr.send(JSON.stringify(lFormData));
+    });
+}
+
+function signupListener() {
+
+    var signupForm = document.getElementById('signupForm');
+    signupForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        var sPasswordInput = document.getElementById('s-password');
+        var confirmPasswordInput = document.getElementById('confirmPassword');
+
+        if (sPasswordInput.value !== confirmPasswordInput.value) {
+            var passwordMismatchMessage = document.getElementById('passwordMismatch');
+            passwordMismatchMessage.style.display = 'block';
+            confirmPasswordInput.value = "";
+            return; 
+        }
+
+        var firstNameInput = document.getElementById('firstName');
+        var lastNameInput = document.getElementById('lastName');
+        var sEmailAddressInput = document.getElementById('s-emailAddress');
+        var sFormData = {
+            firstName: firstNameInput.value,
+            lastName: lastNameInput.value,
+            emailAddress: sEmailAddressInput.value,
+            password: sPasswordInput.value,
+            confirmPassword: confirmPasswordInput.value
+        };
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:5000/signup', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+
+                toggleForm('loginForm');
+                messageDiv.textContent = response.message;
+                messageDiv.style.color = 'black';
+                setTimeout(function () {
+                    messageDiv.style.display = 'none';
+                }, 3000);
+
+            } else {
+                messageDiv.textContent = 'Oops! An error occurred.';
+            }
+        };
+
+        xhr.send(JSON.stringify(sFormData));
+    });
+}
+
+
+function goToHomePage() {
+    var loadingOverlay = document.getElementById('loading-overlay');
+    var form = document.querySelector('.form-container');
+
+    loadingOverlay.style.display = 'block';
+    form.style.opacity = '0.7'
+    setTimeout(function () {
+        window.location.href = 'html/index.html';
+        loadingOverlay.style.display = 'none';
+    }, 2000);
+}
+
+
+function toggleForm(formId) {
+    var loginForm = document.getElementById('loginForm');
+    var signupForm = document.getElementById('signupForm');
+
+    if (formId == loginForm.id) {
+        loginForm.classList.add("active");
+        signupForm.classList.remove("active");
+        signupForm.reset();
+        loginListener();
+    } else if (formId == signupForm.id) {
+        loginForm.classList.remove("active");
+        signupForm.classList.add("active");
+        loginForm.reset();
+        signupListener();
+    }
+}
--- F1 Schedule/python/app.py
+++ F1 Schedule/python/app.py
@@ -1,14 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import mysql.connector
 from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
+
 CORS(app, resources={r"/get_slideshow_images": {"origins": "http://localhost:58186"}, 
                      r"/get_event_thumbnails": {"origins": "http://localhost:58186"},
                      r"/get_event_details": {"origins": "http://localhost:58186"},
-                     r"/get_drivers/*": {"origins": "http://localhost:58186"}
+                     r"/get_drivers/*": {"origins": "http://localhost:58186"},
+                     r"/login": {"origins": "http://localhost:58186"},
+                     r"/signup": {"origins": "http://localhost:58186"}
 })
 
 load_dotenv()
@@ -19,6 +22,66 @@ db = mysql.connector.connect(
     password=os.getenv('DB_PASSWORD'),
     database='f1 schedule'
 )
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email_address = data['emailAddress']
+    password = data['password']
+
+    db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password=os.getenv('DB_PASSWORD'),
+    database='f1 schedule'
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(f"SELECT COUNT(*) FROM users \
+                     WHERE email = '{email_address}' AND password = SHA('{password}');")
+
+    count = cursor.fetchone()
+    
+    if int(count[0]) == 1:
+        response = {'message': 1}
+    else:
+        response = {'message': 0}
+
+    cursor.close()
+    db.close()
+
+    return jsonify(response)
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email_address = data['emailAddress']
+    password = data['password']
+    confirm_password = data['confirmPassword']
+
+    db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password=os.getenv('DB_PASSWORD'),
+    database='f1 schedule'
+    ) 
+
+    cursor = db.cursor()
+
+    cursor.execute(f"INSERT INTO users (fname, lname, email, password) \
+                     VALUES ('{first_name}', '{last_name}', '{email_address}', SHA('{password}'))")
+    
+    response = {'message': 'User created successfully!'}
+
+    db.commit()
+    cursor.close()
+    db.close()
+
+    return jsonify(response)
 
 
 @app.route('/get_slideshow_images', methods=['GET'])
@@ -91,6 +154,6 @@ def get_drivers(event):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
     db.close()
